50d4b0aa20edd6951332da9f26f7cfc8f5db3538
unknown <slucaseduardo@outlook.com>
2019-01-14 21:49:13 -0200

    Added new routes and retrieving data from weather api

--- Starter/app.js
+++ Starter/app.js
@@ -16,6 +16,11 @@ weatherApp.config(function ($routeProvider) {
         controller: 'forecastController'
     })
     
+    .when('/forecast/:days', {
+        templateUrl: 'pages/forecast.htm',
+        controller: 'forecastController'
+    })
+    
 });
 
 // SERVICES
@@ -36,8 +41,26 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
     
 }]);
 
-weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
     
     $scope.city = cityService.city;
+    
+    $scope.days = $routeParams.days || 2;
+    
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+    
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
+    
+    $scope.convertToFahrenheit = function(degK) {
+        
+        return Math.round((1.8 * (degK - 273)) + 32);
+        
+    }
+    
+    $scope.convertToDate = function(dt) { 
+      
+        return new Date(dt * 1000);
+        
+    };
     
 }]);
